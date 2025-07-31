3739d5850fc75a7361cb71995bdb698c1fb7f962
iago bruno <iago.silva6969@gmail.com>
2025-07-31 20:14:53 -0300

    +

--- app.js
+++ app.js
@@ -15,27 +15,27 @@ class App {
     this.routes();
   }
 
-async function criarAluno() {
-  const novoAluno = await prisma.aluno.create({
-    data: {
-      nome: 'João',       // String obrigatória
-      peso: 70.5,         // Float obrigatório
-      // idade é opcional
-      // ativo é automático (default: true)
-      // criadoEm é automático (default: now())
-    }
-  });
-  console.log(novoAluno);
-}
+  async criarAluno() {
+    const novoAluno = await prisma.aluno.create({
+      data: {
+        nome: 'João',       // String obrigatória
+        peso: 70.5,         // Float obrigatório
+        // idade é opcional
+        // ativo é automático (default: true)
+        // criadoEm é automático (default: now())
+      }
+    });
+    console.log(novoAluno);
+  }
 
-middlewares() {
-  this.app.use(express.urlencoded({ extended: true }));
-  this.app.use(express.json());
-}
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
 
-routes() {
-  this.app.use('/', homeRoutes);
-}
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
 }
 
 export default new App().app;
