69b2481df22f056829e3645b7b7c5013a33861a6
iago bruno <iago.silva6969@gmail.com>
2025-07-31 20:10:48 -0300

    adição do prisma ao projeto

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import homeRoutes from './src/routes/homeRoutes.js';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 
 class App {
@@ -9,14 +15,27 @@ class App {
     this.routes();
   }
 
-  middlewares() {
-    this.app.use(express.urlencoded({ extended: true}));
-    this.app.use(express.json());
-  }
+async function criarAluno() {
+  const novoAluno = await prisma.aluno.create({
+    data: {
+      nome: 'João',       // String obrigatória
+      peso: 70.5,         // Float obrigatório
+      // idade é opcional
+      // ativo é automático (default: true)
+      // criadoEm é automático (default: now())
+    }
+  });
+  console.log(novoAluno);
+}
 
-  routes() {
-    this.app.use('/', homeRoutes);
-  }
+middlewares() {
+  this.app.use(express.urlencoded({ extended: true }));
+  this.app.use(express.json());
+}
+
+routes() {
+  this.app.use('/', homeRoutes);
+}
 }
 
 export default new App().app;
