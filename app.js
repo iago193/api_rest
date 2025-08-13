e33d476f24b1a8a089d469d043eb4019a4bd5d5d
iago bruno <iago.silva6969@gmail.com>
2025-08-13 18:11:46 -0300

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from 'path';
+import cors from 'cors';
+import helmet from "helmet";
 
 import homeRoutes from './src/routes/homeRoutes.js';
 import studentRoutes from './src/routes/studentRoutes.js';
@@ -9,6 +11,20 @@ import uploadRoutes from './src/routes/uploadRoutes.js';
 
 const __dirname = path.resolve();
 
+const whiteList = [
+  'http://localhost:3000',
+  'https://iagobruno2.duckdns.org'
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
 class App {
   constructor() {
     this.app = express();
@@ -17,6 +33,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(path.join(__dirname, 'public')));
